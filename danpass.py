a9839072b69e7890a09f5862a2cdfb7b7ceaf0a7
Daniel Cronqvist <dancro99@gmail.com>
2020-10-31 20:15:32 +0100

    can now pretty print an entry

--- danpass.py
+++ danpass.py
@@ -1,4 +1,7 @@
 import crypt
 import passfiles
 
-print(passfiles.load_entries())
+def print_entry(entry):
+    print(f"Website: {entry['site']}")
+    print(f"Username: {entry['username']}")
+    print(f"Password: t{entry['password']}")
