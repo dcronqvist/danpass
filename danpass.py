f927257bb47f8d255981aaedc360ece042eb8f27
Daniel Cronqvist <dancro99@gmail.com>
2020-11-01 00:51:43 +0100

    Added more functionality to commands

--- danpass.py
+++ danpass.py
@@ -3,16 +3,48 @@ import passfiles
 import argparse
 
 parser = argparse.ArgumentParser(description="Manage your passwords!")
-parser.add_argument("--action", metavar='A', help="The action to take, 'find'...")
-parser.add_argument("--site", metavar="S", help="Which site to search for.")
+parser.add_argument("A", metavar='A', help="The action to take, 'find', 'add', 'update' or 'delete'")
+parser.add_argument("-s", "--site", metavar="S", help="Specifies site for action")
+parser.add_argument("-u", "--username", metavar="U", help="Specifies username for action")
+parser.add_argument("-p", "--password", metavar="P", help="Specifies password for action")
+parser.add_argument("-l", "--list", action="store_true", help="Specified if you want to print all found entries")
+parser.add_argument("-id", type=int, default=None, help="Another way to update or delete entries.")
 
 args = parser.parse_args()
 
-def print_entry(entry):
-    print(f"Website: {entry['site']}")
-    print(f"Username: {entry['username']}")
-    print(f"Password: {entry['password']}")
+def print_entry(entry, end=False):
+    print(f"ID: \t\t{entry['id']}")
+    print(f"Website: \t{entry['site']}")
+    print(f"Username: \t{entry['username']}")
+    print(f"Password: \t{entry['password']}")
 
-if args.action and args.site:
-    entry = passfiles.get_entry(args.site)
-    print_entry(entry[0])
+# FIND
+if args.A.lower() == 'find':
+    entries = passfiles.get_entries(args.site)
+    print(f"Found {len(entries)} entries in danpass.")
+    if args.list and len(entries) > 0:
+        print("-----------------------------------")
+        for entry in entries:
+            print_entry(entry)
+            print("-----------------------------------")
+
+# ADD
+if args.A.lower() == 'add' and args.site and args.username and args.password:
+    entry = passfiles.add_entry(args.site, args.username, args.password)
+    print("Added new entry!")
+    print("-----------------------------------")
+    print_entry(entry)
+    print("-----------------------------------")
+
+# DELETE
+if args.A.lower() == 'delete' and ((args.site and args.username and args.password) or args.id >= 0):
+    succ, ent = passfiles.delete_entry(args.site, args.username, args.password, args.id)
+    if succ:
+        print("Successfully deleted entry:")
+        print("-----------------------------------")
+        print_entry(ent)
+        print("-----------------------------------")
+    else:
+        print("Could not find such an entry.")
+
+
--- passfiles.py
+++ passfiles.py
@@ -6,6 +6,7 @@ import json
 """
 [
     {
+        "id": 0,
         "site": "example.com",
         "username": "daniel",
         "password": "coolestguyontheblock"
@@ -35,26 +36,68 @@ def load_entries():
     with open(config.get_setting("passwords-file"), "rb") as file:
         encrypted = file.read()
     # decrypt
-    decrypted = f.decrypt(encrypted)
-    # unjson
-    entries = json.loads(decrypted)
-    return entries
+    if encrypted and encrypted != "":
+        decrypted = f.decrypt(encrypted)
+        # unjson
+        entries = json.loads(decrypted)
+        return entries
+    else:
+        return None
 
 def add_entry(site, username, password):
+    entries = load_entries()
+    new_id = 0
+
+    if entries and len(entries) > 0:
+        new_id = entries[-1]["id"] + 1
+    else:
+        entries = list()
+
     entry = {
+        "id": new_id,
         "site": site,
         "username": username,
         "password": password
     }
-    entries = load_entries()
     entries.append(entry)
     save_entries(entries)
+    return entry
 
-def get_entry(site):
+def delete_entry(site, username, password, e_id):
     entries = load_entries()
-    found_entries = list()
-    for entry in entries:
-        if entry["site"] == site:
-            found_entries.append(entry)
-    return found_entries
+    if e_id >= 0:
+        for entry in entries:
+            if entry["id"] == e_id:
+                entries.remove(entry)
+                save_entries(entries)
+                return True, entry
+
+    if entries and site and username and password:
+        for entry in entries:
+            if entry["site"] == site and entry["username"] == username and entry["password"] == password:
+                # found the correct entry!
+                entries.remove(entry)
+                save_entries(entries)
+                return True, entry
+    return False, None
+
+def update_entry(site, old_username, old_password, new_username, new_password):
+    if delete_entry(site, old_username, old_password, None):
+        return add_entry(site, new_username, new_password)
+    return None
+
+def get_entries(site):
+    entries = load_entries()
+    if entries and site:
+        found_entries = list()
+        for entry in entries:
+            if entry["site"] == site:
+                found_entries.append(entry)
+        return found_entries
+    elif entries and not site:
+        return entries
+    else:
+        return []
+
+
 
